877958a9e370c91c09ea535b8e90936803f4282e
Camila <camila@Dougs-MacBook-Air-5.local>
2022-05-12 19:18:01 +0200

    refactoring

--- js/controls.js
+++ js/controls.js
@@ -1,35 +1,38 @@
-export default function Controls ({
-  buttonPlay,
+export default function Controls({
   buttonPause,
+  buttonPlay,
   buttonSet,
   buttonStop
-  }) {
-  function play (){
-    buttonPlay.classList.add(`hide`);
-    buttonPause.classList.remove(`hide`);
-    buttonSet.classList.add('hide');
-    buttonStop.classList.remove('hide');
+}) {
+
+  function play() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
   }
 
-  function reset (){
-    buttonPlay.classList.remove('hide');
-    buttonPause.classList.add('hide');
-    buttonStop.classList.add('hide');
-    buttonSet.classList.remove('hide');
-  }
-  function pause (){
-    buttonPause.classList.add('hide');
-    buttonPlay.classList.remove('hide');
-  }
-
-  function getMinutes(){
-    let newMinutes = Number(prompt("Quantos minutos ?"));
-    if(!newMinutes) {
-      return false;
-    }
-      return newMinutes;
+  function pause() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
   }
   
+  function reset() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+  }
+
+  function getMinutes() {
+    let newMinutes = prompt('Quantos minutos?')
+    if (!newMinutes) {
+      return false
+    }
+
+    return newMinutes
+  }
+
   return {
     reset,
     play,
--- js/timer.js
+++ js/timer.js
@@ -1,53 +1,68 @@
+import Sounds from "./sounds.js"
 
-export default function Timer ({
-  minutesDisplay,
-  secondsDisplay,
-  timerTimeOut,
-  resetControls,
-  minutes,
-})
-{
-  function updateDisplay (minutes, seconds){
-    minutesDisplay.textContent = String(minutes).padStart(2, "0");
-    secondsDisplay.textContent = String(seconds).padStart(2, "0");
+let sound = Sounds();
+
+export default function Timer({ 
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls
+}) {
+
+  let timerTimeOut
+  let minutes = Number(minutesDisplay.textContent)
+
+  function updateDisplay(newMinutes, seconds) {
+  
+    newMinutes = newMinutes === undefined ? minutes : newMinutes
+    seconds = seconds === undefined ? 0 : seconds
+    minutesDisplay.textContent = String(newMinutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
   }
 
-  function reset(){
-    updateDisplay(minutes,0);
-    clearTimeout(timerTimeOut);
+  function reset() {
+    updateDisplay(minutes, 0)
+    clearTimeout(timerTimeOut)
   }
 
-  function countdown (){
-    timerTimeOut= setTimeout(function(){
-      let seconds = Number(secondsDisplay.textContent);
-      let minutes = Number(minutesDisplay.textContent);
+  function countdown(){
+    timerTimeOut = setTimeout(function() {
+      let seconds =  Number(secondsDisplay.textContent)
+      let minutes = Number(minutesDisplay.textContent)
+      let isFinished = minutes <= 0 && seconds <= 0
 
-      if (seconds <=0 && minutes <= 0){
-        resetControls();
+      updateDisplay(minutes, 0)
+
+      if (isFinished) {
+        resetControls()
+        updateDisplay()
+        sound.timeEnd()
         return
       }
 
-      if (seconds<=0) {
-        seconds=5;
-        minutes = minutes-1;
+      if( seconds <= 0 ) {
+        seconds = 60
+        --minutes
       }
 
-      updateDisplay(minutes, (seconds-1));
+      updateDisplay(minutes, (seconds - 1))
 
-      countdown();
-
+      countdown()
     }, 1000)
-
-    function updateMinutes(newMinutes){
-      minutes = newMinutes;
-    }
   }
 
-  return{
+  function updateMinutes(newMinutes) {
+    minutes = newMinutes
+  }
+
+  function hold() {
+    clearTimeout(timerTimeOut)
+  }
+
+  return {
     countdown,
     reset,
     updateDisplay,
-    updateMinutes
-
+    updateMinutes,
+    hold
   }
 }
